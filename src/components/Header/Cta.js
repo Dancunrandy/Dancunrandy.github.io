494e9e446e2352f629b5b607b1988c60a6340196
dancunrandy <dancunrandy@gmail.com>
2024-06-17 04:04:37 -0400

    removebug

--- src/components/Header/Cta.js
+++ src/components/Header/Cta.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Software  from '../../daniel mutie.pdf'
+import React from 'react';
+// Corrected the path to match the actual file location and name
+import SoftwareDevop from '../../assets/Software devop.pdf';
+
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={Software} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary' >Let's Chat</a>
+      <a href={SoftwareDevop} download className='btn'>Download CV</a>
+      <a href="#contact" className='btn btn-primary'>Let's Chat</a>
     </div>
-  )
-}
+  );
+};
 
-export default Cta
+export default Cta;
